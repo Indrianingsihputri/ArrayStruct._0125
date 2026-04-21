412fa36a5a22f21b5329a12dc72ff6fd4114a224
“indrianingsihputri” <indrianingsihputri30@gmail.com>
2026-04-21 09:35:44 +0700

    menambahkan data int main

--- Struct.4.cpp
+++ Struct.4.cpp
@@ -8,3 +8,17 @@ struct Mahasiswa
     string Alamat;
     int Umur;
 }
+
+int main()
+{
+    Mahasiswa mhs;
+    cout << "Nomor Mahasiswa: ";
+    cin >> mhs.NIM;
+    cout << "Nama Mahasiswa: ";
+    cin.ignore();
+    getline(cin, mhs.Nama);
+    cout << "Alamat Mahasiswa: ";
+    getline(cin, mhs.Alamat);
+    cout << "Umur Mahasiswa: ";
+    cin >> mhs.Umur;
+}

0701b5516498d40cdb0b8ab239a7d1fae3dc09ca
“indrianingsihputri” <indrianingsihputri30@gmail.com>
2026-04-21 10:25:41 +0700

    mengubah nya

--- Struct4.cpp
+++ Struct4.cpp
@@ -24,26 +24,29 @@ int main()
     {
     cout << " Data Mahasiswa ke-" << i + 1 << endl;
     cout << "Nomor Mahasiswa: ";
-    getline(cin, mhs.NIM);
+    getline(cin, mhs[i].NIM);
     cout << "Nama Mahasiswa: ";
     getline(cin, mhs.Nama);
-    cout << "Alamat Mahasiswa: ";
+    cout << "Alamat Mahasiswa => " << endl;
     cout << "\n Nama Desa: ";
-    cin >> mhs.Alamat.desa;
+    cin >> mhs[i].Alamat.desa;
     cout << "\n Nama Kota: ";
-    cin >> mhs.Alamat.kota;
+    cin >> mhs[i].Alamat.kota;
     cout << "Umur Mahasiswa: ";
-    cin >> mhs.Umur;  
+    cin >> mhs[i].Umur;  
     } 
-
+    for (int i = 0; i < 3; i++)
+    {
     cout << endl;
-    cout <<"\n NIM: " << mhs.NIM;
-    cout <<"\n Nama: " << mhs.Nama;
-    cout <<"\n Alamat: " << mhs.Alamat;
-    cout <<"\n Alamat Desa: " << mhs.Alamat.desa;
-    cout <<"\n Alamat Kota: " << mhs.Alamat.kota;
-    cout <<"\n Umur: " << mhs.Umur;
+    cout <<"\n NIM: " << mhs[0].NIM;
+    cout <<"\n Nama: " << mhs[0].Nama;
+    cout <<"\n Alamat: " << mhs[0].Alamat;
+    cout <<"\n Alamat Desa: " << mhs[0].Alamat.desa;
+    cout <<"\n Alamat Kota: " << mhs[0].Alamat.kota;
+    cout <<"\n Umur: " << mhs[0].Umur;
+    }
 
+    
     return 0;
    
 }

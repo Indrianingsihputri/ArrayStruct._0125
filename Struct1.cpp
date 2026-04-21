bb445c9c0b2576095642a91ba1cd4b3e87cb9b42
“indrianingsihputri” <indrianingsihputri30@gmail.com>
2026-04-21 09:39:21 +0700

    full code Struct1

---
+++ Struct1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+struct Mahasiswa
+{
+    string NIM;
+    string Nama;
+    string Alamat;
+    int Umur;
+}
+
+int main()
+{
+    Mahasiswa mhs;
+    cout << "Nomor Mahasiswa: ";
+    cin >> mhs.NIM;
+    cout << "Nama Mahasiswa: ";
+    cin.ignore();
+    getline(cin, mhs.Nama);
+    cout << "Alamat Mahasiswa: ";
+    getline(cin, mhs.Alamat);
+    cout << "Umur Mahasiswa: ";
+    cin >> mhs.Umur;
+
+    cout << endl;
+    cout <<"\n NIM: " << mhs.NIM;
+    cout <<"\n Nama: " << mhs.Nama;
+    cout <<"\n Alamat: " << mhs.Alamat;
+    cout <<"\n Umur: " << mhs.Umur;
+}
